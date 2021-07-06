9462894491a30a9b74074891747702e8bba9b0c1
Grey41 <85015029+Grey41@users.noreply.github.com>
2021-07-06 15:44:51 +0100

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,22 +1,20 @@
-window.addEventListener("DOMContentLoaded", loaded)
-
-function loaded() {
+window.addEventListener("DOMContentLoaded", () => {
 	document.body.innerHTML = document.body.innerHTML
 	.replace(/¦/g, "<font color = '77b13e'>")
 	.replace(/%/g, "<font color = '999999'>")
 	.replace(/{/g, "<font color = '3e77b1'>")
 	.replace(/}/g, "</font>")
 	.replace("^", new Date().getFullYear().toString())
+})
 
-    window.onscroll = function() {
-		if (document.scrollingElement.scrollTop > 100) $(".back").fadeIn()
-		else $(".back").fadeOut()
-	}
-}
-
-function bar() {
+const bar = () => {
 	let top = document.querySelector(".top")
 	
 	if (top.className === "top") top.className += " responsive"	
 	else top.className = "top"
 }
+
+window.dataLayer = window.dataLayer || []
+const gtag = () => dataLayer.push(arguments)
+gtag('js', new Date())
+gtag('config', 'G-W2C6ZR2P0X')
