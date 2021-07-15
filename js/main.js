47abf7cc849c43b924038944cf7ce13bd8c7caaa
Grey41 <85015029+Grey41@users.noreply.github.com>
2021-07-15 10:23:46 +0100

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,10 +1,62 @@
 window.addEventListener("DOMContentLoaded", () => {
-	document.body.innerHTML = document.body.innerHTML
-	.replace(/¦/g, "<font color = '77b13e'>")
-	.replace(/%/g, "<font color = '999999'>")
-	.replace(/{/g, "<font color = '3e77b1'>")
-	.replace(/}/g, "</font>")
-	.replace("^", new Date().getFullYear().toString())
+	const footers = document.getElementsByTagName("footer")
+
+	for (footer of footers)
+		footer.innerText = "\u00a9 Copyright " + new Date().getFullYear() + " GreyHope"
+
+	if (typeof ace === "undefined") return
+
+	const iframes = document.querySelectorAll(".iframe")
+	const editors = document.querySelectorAll(".editor")
+	const list = document.querySelectorAll(".example")
+	const demos = Object.values(iframes)
+	const examples = Object.values(list)
+
+	const input = (demo) => {
+		const frame = document.createElement("iframe")
+		frame.className = "iframe"
+
+		demo.iframe.before(frame)
+		demo.iframe.remove()
+		demo.iframe = frame
+
+		const content = demo.iframe.contentWindow || demo.iframe.contentDocument
+		content.document.open()
+		content.document.write(demo.ace.getValue())
+		content.document.close()
+	}
+
+	demos.forEach((iframe, index) => {
+		const span = document.createElement("span")
+		span.className = "note"
+		span.innerText = "Edit the code to change the result above."
+
+		const editor = Object.values(editors)[index]
+		editor.after(span)
+
+		demos[index] = {iframe, editor, ace: ace.edit(editor), hold: false}
+		demos[index].ace.session.setMode("ace/mode/html")
+		demos[index].ace.setTheme("ace/theme/monokai")
+		demos[index].ace.setValue(demos[index].ace.getValue().trim())
+		demos[index].ace.on("change", () => input(demos[index]))
+		input(demos[index])
+
+		document.addEventListener("mousedown", () => demos[index].hold = true)
+		document.addEventListener("mouseup", () => demos[index].hold = false)
+		document.addEventListener("mousemove", () => demos[index].hold && demos[index].ace.resize())
+	})
+
+	examples.forEach(example => {
+		const editor = ace.edit(example)
+
+		console.log(editor.getValue())
+
+		editor.session.setMode("ace/mode/javascript")
+		editor.setTheme("ace/theme/monokai")
+		editor.setValue(editor.getValue().trim())
+		editor.setReadOnly(true)
+		editor.setOptions({maxLines: Infinity})
+	})
 })
 
 const bar = () => {
